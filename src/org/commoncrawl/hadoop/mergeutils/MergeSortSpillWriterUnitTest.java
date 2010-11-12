d536eac87fa092f953f8ee5b93955ec0f8c978a6
ahadrana <ahadrana@gmail.com>
2010-11-11 22:52:59 -0800

    Crap! Fixed double check-in and missing license stuff

--- src/org/commoncrawl/hadoop/mergeutils/MergeSortSpillWriterUnitTest.java
+++ src/org/commoncrawl/hadoop/mergeutils/MergeSortSpillWriterUnitTest.java
@@ -1,8 +1,24 @@
 package org.commoncrawl.hadoop.mergeutils;
 
+/*
+*		 Copyright 2010 - CommonCrawl Foundation
+*
+*    Licensed under the Apache License, Version 2.0 (the "License");
+*    you may not use this file except in compliance with the License.
+*    You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+*    Unless required by applicable law or agreed to in writing, software
+*    distributed under the License is distributed on an "AS IS" BASIS,
+*    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*    See the License for the specific language governing permissions and
+*    limitations under the License.
+*/
+
+
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.util.Collections;
 import java.util.Random;
 import java.util.TreeMap;
 
@@ -13,10 +29,7 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DataInputBuffer;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.Writable;
-import org.apache.hadoop.io.WritableComparable;
 import org.commoncrawl.util.shared.CCStringUtils;
 
 /**
